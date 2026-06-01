152feff392d79f3a290119fac44ef9b11e5f6de7
Alexander S. <1203110+xsynaptic@users.noreply.github.com>
2026-06-01 22:40:20 +0800

    chore: additional playground targets

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
 	integrations: [
 		fontDevtools({
 			providers: ['google', 'fontsource', 'bunny', 'fontshare'],
-			targets: ['--font-heading', 'h1', 'p'],
+			targets: ['--font-heading', 'h1', 'p', 'code', '.additional-notes'],
 		}),
 	],
 });
